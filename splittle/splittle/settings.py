176ce22f8fa7351fe33b9165bc474898c3609340
tanukiJami <bilaljami21@gmail.com>
2024-09-15 04:24:47 -0400

    Updated leave functionality.

--- splittle/splittle/settings.py
+++ splittle/splittle/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3_2i37eme)ncqnks#*9k=q&1$pn%@1%0omlg%c0*!ntwlga(n('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
